49802d1f7bfec40f0a6e76d252290ac156a7f5c1
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-05-27 13:57:39 +0700

    second commit

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -6,6 +6,8 @@ import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerController {
@@ -21,5 +23,13 @@ public class CustomerController {
     public String create(Model model){
         model.addAttribute("customer", new Customer());
         return "create";
+    }
+
+    @PostMapping("/customer/save")
+    public String save(Customer customer, RedirectAttributes redirectAttributes){
+        customer.setId((int)(Math.random() * 1000));
+        customerService.save(customer);
+        redirectAttributes.addFlashAttribute("success", "Saved customer successfully!");
+        return "redirect";
     }
 }
